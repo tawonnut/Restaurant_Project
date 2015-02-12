32cb8b35d93a3bb773488d1ac3fa0211f20ec1dc
kiattisakooooo <tawonnut35@gmail.com>
2015-02-12 14:43:07 +0700

    Bug mongoid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
 
-
-
-
+  def current_user=(user)
+	  @current_user = user
+	  session[:user_id] = user.try(:id).try(:to_s)
+    end
 end
+
