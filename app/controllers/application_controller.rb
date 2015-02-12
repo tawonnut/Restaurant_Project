0910bb822210d643daef1682ab4fe376ea906071
kiattisakooooo <tawonnut35@gmail.com>
2015-02-12 15:12:40 +0700

    Before_action:authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_sesefore_action :authenticate_user!
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   protect_from_forgery with: :exception
 
   def current_user=(user)
